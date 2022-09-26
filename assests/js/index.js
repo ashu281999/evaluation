248ced3be21cffe56245cd67251982205d33114c
ashu281999 <113037674+ashu281999@users.noreply.github.com>
2022-09-26 12:16:14 +0530

    Add files via upload
    first commit

---
+++ assests/js/index.js
@@ -0,0 +1,13 @@
+
+const s =  document.getElementById("lol");
+function menuc()
+{  
+
+    if(s.style.display == "none")
+     {
+        s.style.display="block";
+     } 
+     else{
+        s.style.display="none";
+     }
+}
